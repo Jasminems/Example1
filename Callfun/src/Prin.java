3611df8a26a6e671b9c96eb4765a48f6733d19c0
Jasminems <jasmine.srakj@gmail.com>
2021-07-03 02:43:21 +0530

    Calling function name

---
+++ Callfun/src/Prin.java
@@ -0,0 +1,12 @@
+/*A Java program to print name using method*/
+class Prin 
+{
+	static void Printname()	
+	{
+		System.out.println("Jasmine");
+	}
+	public static void main(String[] args) 
+	{
+		Printname();
+	}
+}
