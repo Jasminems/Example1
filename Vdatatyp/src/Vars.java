806d340e23809eff612b457d8866acba2942f0da
Jasminems <jasmine.srakj@gmail.com>
2021-07-03 02:39:47 +0530

    Types of Data Type

---
+++ Vdatatyp/src/Vars.java
@@ -0,0 +1,20 @@
+/*A Java program for declaration of variables*/
+class Vars 
+{
+	static int n=5;
+	static float f=10.99f;
+	static double d=19.99d;
+	static char c='J';
+	static boolean val=true;
+	static boolean val1=false;
+	public static void main(String[] args) 
+	{
+		System.out.println("Types of Variables");
+		System.out.println("Integer value:"+n);
+		System.out.println("Float value:"+f);
+		System.out.println("Double value:"+d);
+		System.out.println("Character value:"+c);
+		System.out.println("Boolean value:"+val);
+		System.out.println("Boolean value:"+val1);
+	}
+}
