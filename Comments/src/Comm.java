44a651df39b50b91e25d6f9b4cf025ccfa9d8fd5
Jasminems <jasmine.srakj@gmail.com>
2021-07-03 02:38:05 +0530

    Comments

---
+++ Comments/src/Comm.java
@@ -0,0 +1,11 @@
+/*A Java program for a single line,multi-line and documentation comments*/
+class Comm 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("//This is a single line comment");
+		System.out.println("/*Multline comments is used to explain the code*/");
+		System.out.println("/**Java documentation*/");
+				
+	}
+}
